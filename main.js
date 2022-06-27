1131739bca5dcc68c73dc7dca76da93736e7c8d6
Jacob Britton <jacobbritton95@gmail.com>
2022-06-27 17:49:58 -0400

    Implement operate function

--- main.js
+++ main.js
@@ -1,19 +1,34 @@
-function add(num1, num2) {
+const add = function(num1, num2) {
     return num1 + num2;
 }
 
-function sub(num1, num2) {
+const sub = function(num1, num2) {
     return num1 - num2;
 }
 
-function mul(num1, num2) {
+const mul = function(num1, num2) {
     return num1 * num2;
 }
 
-function div(num1, num2) {
+const div = function(num1, num2) {
     if (num2 === 0) {
-        return "Error: division by zero."
+        return "ERROR"
     } else {
         return num1 / num2;
     }
 }
+
+const operate = function(num1, num2, op) {
+    if (op === 'sum') {
+        return sum(num1, num2);
+
+    } else if (op === 'sub') {
+        return sub(num1, num2);
+
+    } else if (op === 'mul') {
+        return mul(num1, num2);
+
+    } else if (op === 'div') {
+        return div(num1, num2);
+    }
+};
