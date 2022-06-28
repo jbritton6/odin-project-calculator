cab358a1fd491569af20e5574a3996b1d36caa67
Jacob Britton <jacobbritton95@gmail.com>
2022-06-27 23:30:14 -0400

    Implement some logic
    Logic for a sequence of operations implemented and briefly
    tested.  An example would include:
       1 + 2 = 3 => 3 - 1 = 2
                 => 2 x 4 = 8
                 => 8 / 4 = 2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const add = function(num1, num2) {
+const sum = function(num1, num2) {
     return num1 + num2;
 }
 
@@ -32,3 +32,65 @@ const operate = function(num1, num2, op) {
         return div(num1, num2);
     }
 };
+
+
+function addClickEvents() {
+    buttonIds = [
+        'button-0', 'button-1', 'button-2', 'button-3', 'button-4', 
+        'button-5', 'button-6', 'button-7', 'button-8', 'button-9',
+        'button-sum', 'button-sub', 'button-mul', 'button-div',
+        'button-eql', 'button-clr'
+    ];
+    for (let id of buttonIds) {
+        document.querySelector(`#${id}`)
+            .addEventListener('click', () => onClick(id.split('-')[1]));
+    }
+}
+
+initalize();
+addClickEvents();
+let firstVal = 0;
+let secondVal = 0;
+let firstValSet = false;
+let secondValSet = false;
+let operation = '';
+let operationResult = 0;
+let clearNextClick = false;
+let stack = [];
+
+function initalize() {
+    document.querySelector('#display').innerHTML = '0';
+}
+
+function onClick(arg) {
+    let numbers = '0123456789';
+    let operations = ['sum', 'sub', 'mul', 'div']
+    let display = document.querySelector('#display');    
+
+    if (clearNextClick) {
+        display.innerHTML = '0';
+        clearNextClick = false;
+    }
+
+    if (numbers.includes(arg)) {
+        display.innerHTML += arg;
+    }
+
+    if (operations.includes(arg)) {
+        if (stack.length === 0) {
+            stack.push(Number(display.innerHTML));
+            stack.push(arg);
+            clearNextClick = true;
+
+        } else if (stack.length === 2) {
+            stack.push(Number(display.innerHTML));
+            operationResult = operate(stack[0], stack[2], stack[1]);
+            stack.length = 0;
+            stack.push(operationResult);
+            stack.push(arg);
+            display.innerHTML = operationResult;
+            clearNextClick = true;
+        }
+    }
+
+}
